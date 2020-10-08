961ba189931ba4359acc249dce87d4cc338fa5d6
Joel Holmes <holmes89@gmail.com>
2020-10-07 20:14:48 -0400

    partial working

--- repl/parser.go
+++ repl/parser.go
@@ -15,8 +15,7 @@ type Parser struct {
 	lex *Lexer
 }
 
-// New returns new parser
-func New(lex *Lexer) *Parser {
+func NewParser(lex *Lexer) *Parser {
 	return &Parser{lex}
 }
 
@@ -29,6 +28,15 @@ func (p *Parser) Parse() (*Expr, error) {
 		return p.quote()
 	case tokenAtom, tokenConst, tokenNumber:
 		return atomExpr(token), nil
+	case tokenLpar:
+		expr, err := p.list()
+		if err != nil {
+			return nil, err
+		}
+		token = p.lex.NextToken()
+		if token.typ == tokenRpar {
+			return expr, nil
+		}
 	}
 
 	return nil, fmt.Errorf("bad token in expression: %q", token)
@@ -40,6 +48,48 @@ func (p *Parser) quote() (*Expr, error) {
 		return nil, err
 	}
 	return cons(atomExpr(tokenQuoteWord), cons(exp, nil)), nil
+}
+
+func (p *Parser) list() (*Expr, error) {
+	ptok := p.lex.Peek()
+
+	switch ptok {
+	case ')':
+		return nil, nil
+	case '(':
+		exp, err := p.Parse()
+		if err != nil {
+			return nil, err
+		}
+		l, err := p.list()
+		if err != nil {
+			return nil, err
+		}
+		return cons(exp, l), nil
+	}
+
+	tok := p.lex.NextToken()
+	switch tok.typ {
+	case tokenQuote:
+		q, err := p.quote()
+		if err != nil {
+			return nil, err
+		}
+		l, err := p.list()
+		if err != nil {
+			return nil, err
+		}
+		return cons(q, l), nil
+	case tokenAtom, tokenConst, tokenNumber:
+		l, err := p.list()
+		if err != nil {
+			return nil, err
+		}
+		return cons(atomExpr(tok), l), nil
+	case tokenDot:
+		return p.Parse()
+	}
+	return nil, fmt.Errorf("bad token in expression: %q", tok)
 }
 
 func car(e *Expr) *Expr {
--- repl/repl.go
+++ repl/repl.go
@@ -1,18 +1,31 @@
 package repl
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"strings"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
 	for {
-		fmt.Fprint(out, PROMPT)
-		l := NewLexer(in)
-		for tok := l.NextToken(); tok.typ != tokenEOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
 		}
+
+		line := scanner.Text()
+		p := NewParser(NewLexer(strings.NewReader(line)))
+		exp, err := p.Parse()
+		if err != nil {
+			fmt.Fprintf(out, "%+v\n", err)
+			continue
+		}
+		fmt.Fprintf(out, "%+v\n", exp)
 	}
 }
